ebc24bd5902662e3928d7c8ccaa03e9bbb9c3c16
RekaSafira17A1 <37066840+RekaSafira17A1@users.noreply.github.com>
2018-04-14 01:40:18 -1200

    Add files via upload

---
+++ input.output.py
@@ -0,0 +1,13 @@
+print("                   Input dan Output")
+print("================================================== \n")
+
+nm = raw_input("Nama :")
+nim=input("NIM  :")
+kls=raw_input("Kelas :")
+print"\n\n"
+
+print "DATA MAHASISWA\n"
+
+print (nm)
+print(nim)
+print(kls)
